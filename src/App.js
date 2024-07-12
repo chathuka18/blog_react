b9d48d934f9c978fbdb73ec6c604de421aa089c9
chathuka18 <147928535+chathuka18@users.noreply.github.com>
2024-07-12 14:36:42 +0530

    chore: Add initial project files and configuration

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import Home from './Home';
+import Navbar from './Navbar';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Craete from './Craete';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        
+        <div className="content">
+          <Switch>
+            <Route  exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Craete />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+
+          </Switch>
+          
+        </div>
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
